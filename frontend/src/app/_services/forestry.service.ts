cbe0d7b3335184f22d0c9082f9681e15dd46c537
Wydzials <48773794+Wydzials@users.noreply.github.com>
2021-10-27 23:40:17 +0200

    Dodanie wstępnej wersji listy leśnictw

--- frontend/src/app/_services/forestry.service.ts
+++ frontend/src/app/_services/forestry.service.ts
@@ -8,3 +8,27 @@ export class ForestryService {
   constructor() {
   }
 }
+
+export interface ForestDistrict {
+  forest_district_id: number
+  forest_manager: Employee
+  name: string
+  forestries: Forestry[]
+}
+
+export interface Forestry {
+  forestry_id: number;
+  forest_district_id: number;
+  forest_district_name: string;
+  forester: string; // todo -> Employee
+  name: string;
+  area: number;
+}
+
+export interface Employee {
+  employee_id: number;
+  first_name: string;
+  last_name: string;
+  email: string;
+  phone_number: string;
+}
--- frontend/src/app/core/forestry-list/forestry-list.component.ts
+++ frontend/src/app/core/forestry-list/forestry-list.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Forestry} from "../../_services/forestry.service";
 
 @Component({
   selector: 'app-forestry-list',
@@ -6,6 +7,33 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./forestry-list.component.scss'],
 })
 export class ForestryListComponent implements OnInit {
+
+  forestries: Forestry[] = [
+    {
+      forestry_id: 1,
+      forest_district_id: 1,
+      forest_district_name: "Nadleśnictwo 1",
+      forester: "Grzegorz Brzęczyszczykiewicz",
+      name: "Leśnictwo 1",
+      area: 100
+    },
+    {
+      forestry_id: 2,
+      forest_district_id: 1,
+      forest_district_name: "Nadleśnictwo 1",
+      forester: "Grzegorz Brzęczyszczykiewicz",
+      name: "Leśnictwo 2",
+      area: 200
+    },
+    {
+      forestry_id: 3,
+      forest_district_id: 1,
+      forest_district_name: "Nadleśnictwo 1",
+      forester: "Grzegorz Brzęczyszczykiewicz",
+      name: "Leśnictwo 3",
+      area: 300
+    },
+  ];
 
   constructor() {
   }
