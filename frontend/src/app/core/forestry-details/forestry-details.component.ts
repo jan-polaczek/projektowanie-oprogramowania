e0612947f83da742f61a9da9508219939a938810
jurgas <Sebastianjurga7@gmail.com>
2021-10-27 20:40:54 +0200

    dodanie podstawowej struktury plikow

---
+++ frontend/src/app/core/forestry-details/forestry-details.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-forestry-details',
+  templateUrl: './forestry-details.component.html',
+  styleUrls: ['./forestry-details.component.scss'],
+})
+export class ForestryDetailsComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ frontend/src/app/core/forestry-list/forestry-list.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-forestry-list',
+  templateUrl: './forestry-list.component.html',
+  styleUrls: ['./forestry-list.component.scss'],
+})
+export class ForestryListComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
